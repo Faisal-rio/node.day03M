d3f8ebeffd220be8579a76174898a2b82c773b80
Faisal-rio <faisal.sfzubaida@gmail.com>
2024-09-08 14:30:46 -0700

    commit

--- mentor-student-api/index.js
+++ mentor-student-api/index.js
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
 });
 
 // Connect to MongoDB using environment variable
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Error connecting to MongoDB:', err));
 
